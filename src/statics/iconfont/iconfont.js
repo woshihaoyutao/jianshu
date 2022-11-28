46c92b04cb93db1e53be28a1e5dc789d7c4daa98
hyt <1151123047@qq.com>
2022-11-28 16:00:12 +0800

    使用iconfont嵌入头部图标已实现

---
+++ src/statics/iconfont/iconfont.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+createGlobalStyle`
+@font-face {
+  font-family: "iconfont"; /* Project id 3793152 */
+  src: url('iconfont.eot?t=1669384914613'); /* IE9 */
+  src: url('iconfont.eot?t=1669384914613#iefix') format('embedded-opentype'), /* IE6-IE8 */
+       url('iconfont.woff2?t=1669384914613') format('woff2'),
+       url('iconfont.woff?t=1669384914613') format('woff'),
+       url('iconfont.ttf?t=1669384914613') format('truetype'),
+       url('iconfont.svg?t=1669384914613#iconfont') format('svg');
+}
+
+.iconfont {
+  font-family: "iconfont" !important;
+  font-size: 16px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+`;
